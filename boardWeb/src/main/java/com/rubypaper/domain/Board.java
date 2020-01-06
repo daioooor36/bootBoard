5d3e125606bca1a9b8de14e22dd4c957028e58d4
daioooor36 <pd6016@naver.com>
2020-01-06 13:25:54 +0900

    init Source : 프로젝트 세팅

---
+++ boardWeb/src/main/java/com/rubypaper/domain/Board.java
@@ -0,0 +1,90 @@
+package com.rubypaper.domain;
+
+import java.util.Date;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@ToString
+@Entity
+public class Board {
+	
+	@Id @GeneratedValue
+	private Long seq;
+	
+	private String title;
+	
+	@Column(updatable=false)
+	private String writer;
+	
+	private String content;
+	
+	@Column(insertable=false, updatable=false, columnDefinition="date default sysdate")
+	private Date createDate;
+	
+	@Column(insertable=false, updatable=false, columnDefinition="number default 0")
+	private Long cnt;
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((cnt == null) ? 0 : cnt.hashCode());
+		result = prime * result + ((content == null) ? 0 : content.hashCode());
+		result = prime * result + ((createDate == null) ? 0 : createDate.hashCode());
+		result = prime * result + ((seq == null) ? 0 : seq.hashCode());
+		result = prime * result + ((title == null) ? 0 : title.hashCode());
+		result = prime * result + ((writer == null) ? 0 : writer.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Board other = (Board) obj;
+		if (cnt == null) {
+			if (other.cnt != null)
+				return false;
+		} else if (!cnt.equals(other.cnt))
+			return false;
+		if (content == null) {
+			if (other.content != null)
+				return false;
+		} else if (!content.equals(other.content))
+			return false;
+		if (createDate == null) {
+			if (other.createDate != null)
+				return false;
+		} else if (!createDate.equals(other.createDate))
+			return false;
+		if (seq == null) {
+			if (other.seq != null)
+				return false;
+		} else if (!seq.equals(other.seq))
+			return false;
+		if (title == null) {
+			if (other.title != null)
+				return false;
+		} else if (!title.equals(other.title))
+			return false;
+		if (writer == null) {
+			if (other.writer != null)
+				return false;
+		} else if (!writer.equals(other.writer))
+			return false;
+		return true;
+	}
+}
